cf60970c432a8ba740efb668d1b962bd76ec25eb
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-02-10 17:09:21 +0330

    express-hw6-comments added

--- hw13-express-6/app.js
+++ hw13-express-6/app.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require('path');
 const fs=require('fs');
 app.use('/', (req, res) => {
-    if(fs.existsSync(path.join(__dirname, 'pages',req.url)))
-        res.sendFile(path.join(__dirname, 'pages',req.url))
+    if(fs.existsSync(path.join(__dirname, 'pages',req.url))) // check if file exists
+        res.sendFile(path.join(__dirname, 'pages',req.url)) //return file as response
     else
         res.end("Path not found!");
 }).listen(5000, () => {
